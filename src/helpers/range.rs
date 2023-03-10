f15045716c7c5bb0e71411bd23a74815dae1d92d
txhsl <799498265@qq.com>
2023-03-10 17:10:22 +0800

    format code

--- src/helpers/range.rs
+++ src/helpers/range.rs
@@ -41,37 +41,37 @@ impl Range{
         let a_var = Scalar::from(a.unwrap().0);
         let b_var = Scalar::from(b.unwrap().0);
         let w_var = Scalar::from(w.unwrap().0);
-        let mut a_v=match a.unwrap().1 {
-            true =>cs.alloc(|| "a", ||  Ok(a_var))?,
-            false =>cs.alloc_input(|| "a", ||  Ok(a_var))?,
+        let mut a_v = match a.unwrap().1 {
+            true => cs.alloc(|| "a", ||  Ok(a_var))?,
+            false => cs.alloc_input(|| "a", ||  Ok(a_var))?,
         };
-        let mut b_v=match b.unwrap().1 {
-            true =>cs.alloc(|| "b", || Ok(b_var))?,
-            false =>cs.alloc_input(|| "b", || Ok(b_var))?,
+        let mut b_v = match b.unwrap().1 {
+            true => cs.alloc(|| "b", || Ok(b_var))?,
+            false => cs.alloc_input(|| "b", || Ok(b_var))?,
         };
-        let mut w_v=match w.unwrap().1 {
-            true =>cs.alloc(|| "w", || Ok(w_var))?,
-            false =>cs.alloc_input(|| "w", || Ok(w_var))?,
+        let mut w_v = match w.unwrap().1 {
+            true => cs.alloc(|| "w", || Ok(w_var))?,
+            false => cs.alloc_input(|| "w", || Ok(w_var))?,
         };
 
         let not_all_zeros_var = Scalar::from(not_all_zeros.unwrap().0);
-        let mut not_all_zeros_v=match not_all_zeros.unwrap().1 {
-            true =>cs.alloc(|| "not_all_zeros", || Ok(not_all_zeros_var))?,
-            false =>cs.alloc_input(|| "not_all_zeros", || Ok(not_all_zeros_var))?,
+        let mut not_all_zeros_v = match not_all_zeros.unwrap().1 {
+            true => cs.alloc(|| "not_all_zeros", || Ok(not_all_zeros_var))?,
+            false => cs.alloc_input(|| "not_all_zeros", || Ok(not_all_zeros_var))?,
         };
-        let mut wArray_var= vec![];
+        let mut wArray_var = vec![];
         for i in 0 .. wArray.unwrap().0.len(){
-            let mut wArray_v=match wArray.unwrap().1 {
-                true =>cs.alloc(||"",||Ok(Scalar::from(*wArray.unwrap().0.get(i).unwrap()))),
-                false =>cs.alloc_input(||"",||Ok(Scalar::from(*wArray.unwrap().0.get(i).unwrap()))),
+            let mut wArray_v = match wArray.unwrap().1 {
+                true => cs.alloc(|| "", || Ok(Scalar::from(*wArray.unwrap().0.get(i).unwrap()))),
+                false => cs.alloc_input(|| "", || Ok(Scalar::from(*wArray.unwrap().0.get(i).unwrap()))),
             };
             wArray_var.push(wArray_v.unwrap());
         }
-        let mut crArray_var= vec![];
+        let mut crArray_var = vec![];
         for i in 0 .. crArray.unwrap().0.len() {
-            let mut cArray_v=match crArray.unwrap().1 {
-                true =>cs.alloc(||"",||Ok(Scalar::from(*crArray.unwrap().0.get(i).unwrap()))),
-                false =>cs.alloc_input(||"",||Ok(Scalar::from(*crArray.unwrap().0.get(i).unwrap()))),
+            let mut cArray_v = match crArray.unwrap().1 {
+                true => cs.alloc(|| "", || Ok(Scalar::from(*crArray.unwrap().0.get(i).unwrap()))),
+                false => cs.alloc_input(|| "", || Ok(Scalar::from(*crArray.unwrap().0.get(i).unwrap()))),
             };
             crArray_var.push(cArray_v.unwrap());
         }
@@ -79,38 +79,49 @@ impl Range{
         Ok(Range {
             a: a_v,
             b: b_v,
-            w:w_v,
-            wArray:wArray_var,
-            less_or_equal:less_or_equal,
-            less:less,
-            not_all_zeros:not_all_zeros_v,
-            crArray:crArray_var,
+            w: w_v,
+            wArray: wArray_var,
+            less_or_equal: less_or_equal,
+            less: less,
+            not_all_zeros: not_all_zeros_v,
+            crArray: crArray_var,
         })
     }
+}
 
 pub fn less_or_equal<Scalar, CS>(mut cs: CS,a:(u64,bool),b:(u64,bool)) -> Result<(), SynthesisError>
     where
         Scalar: PrimeField,
         CS: ConstraintSystem<Scalar>,
 {
-    let w= ((1<<(64-1u64))+(b.0-a.0)) as u64;
-    let mut wArray=[0].repeat(64);
-    let mut i=0;
-    let mut values=w.clone();
+    let w = ((1 << (64 - 1u64)) + (b.0 - a.0)) as u64;
+    let mut wArray = [0].repeat(64);
+    let mut i = 0;
+    let mut values = w.clone();
     while i < wArray.len() {
-        wArray[i]=values & 1;
-        values>>=1;
+        wArray[i] = values & 1;
+        values >>= 1;
         i += 1;
     }
-    let mut cArray=[0].repeat(64);
-    cArray[0]=wArray[0];
-    let j=1;
+    let mut cArray = [0].repeat(64);
+    cArray[0] = wArray[0];
+    let j = 1;
     while j < wArray.len(){
-        cArray[j]=u64::from(wArray[j]|cArray[j-1]);
+        cArray[j] = u64::from(wArray[j] | cArray[j - 1]);
     }
-    let not_all_zeros=cArray.last().unwrap().clone();
-    let r=Range::alloc(cs.namespace(|| "less_or_equal_alloc"),Some(a),Some(b),Some((w,a.1&b.1)),Some((wArray.try_into().unwrap(),a.1&b.1)),Some((cArray.try_into().unwrap(),a.1&b.1)),1,1,Some((not_all_zeros,a.1&b.1))).expect("range alloc error");
-    return Range::range(cs.namespace(|| "less_or_equal_alloc"),&r);
+    let not_all_zeros = cArray.last().unwrap().clone();
+    let r = Range::alloc(
+        cs.namespace(|| "less_or_equal_alloc"),
+        Some(a),
+        Some(b),
+        Some((w, a.1 & b.1)),
+        Some((wArray.try_into().unwrap(), a.1 & b.1)),
+        Some((cArray.try_into().unwrap(), a.1 & b.1)),
+        1,
+        1,
+        Some((not_all_zeros, a.1 & b.1))
+    ).expect("range alloc error");
+    return range(cs.namespace(|| "less_or_equal_alloc"), &r);
 }
 
 pub fn less<Scalar, CS>(mut cs: CS, input: &Range, ) -> Result<(), SynthesisError>
@@ -145,28 +156,28 @@ pub fn large<Scalar, CS>(
 
 pub fn range<Scalar, CS>(mut cs: CS, input: &Range) -> Result<(), SynthesisError> where Scalar: PrimeField, CS: ConstraintSystem<Scalar>,
 {
-    let a=input.a;
-    let b=input.b;
-    let n=input.wArray.len() as u64;
-    let w=input.w;
-    let exp2n =1<<(n-1);
-    let wArray=&input.wArray;
-    let crArray=&input.crArray;
-    let not_all_zeros=input.not_all_zeros;
-    let less_or_equal= input.less_or_equal;
-    let less=input.less;
+    let a = input.a;
+    let b = input.b;
+    let n = input.wArray.len() as u64;
+    let w = input.w;
+    let exp2n = 1 << (n - 1);
+    let wArray = &input.wArray;
+    let crArray = &input.crArray;
+    let not_all_zeros = input.not_all_zeros;
+    let less_or_equal = input.less_or_equal;
+    let less = input.less;
     cs.enforce(
         || "w=2^n+b-a",
         |lc| lc + w,
         |lc| lc + CS::one(),
-        |lc| lc+(Scalar::from(exp2n), CS::one())+b-a,
+        |lc| lc + (Scalar::from(exp2n), CS::one()) + b - a,
     );
 
     let mut lct = LinearCombination::<Scalar>::zero();
     for i in 0..wArray.len(){
-        lct = lct + (Scalar::from(1<<i), wArray[i]);
+        lct = lct + (Scalar::from(1 << i), wArray[i]);
     }
-    lct = lct -w;
+    lct = lct - w;
     cs.enforce(
         || "2^0*w0+.......-w=0",
         |_| lct,
@@ -178,7 +189,7 @@ pub fn range<Scalar, CS>(mut cs: CS, input: &Range) -> Result<(), SynthesisError
         cs.enforce(
             || "w0(1-w0)=0",
             |lc| lc + wArray[i],
-            |lc| lc + CS::one()-wArray[i],
+            |lc| lc + CS::one() - wArray[i],
             |lc| lc,
         );
     }
@@ -187,15 +198,15 @@ pub fn range<Scalar, CS>(mut cs: CS, input: &Range) -> Result<(), SynthesisError
         || "w0=cr0",
         |lc| lc + wArray[0],
         |lc| lc + CS::one(),
-        |lc| lc+crArray[0],
+        |lc| lc + crArray[0],
     );
 
     for i in 1..crArray.len() {
         cs.enforce(
             || "(cr_(i-1)-1)(wi-1)=1-cr_i",
-            |lc| lc + crArray[i-1]-CS::one(),
-            |lc| lc + wArray[i]-CS::one(),
-            |lc| lc+CS::one()- crArray[i],
+            |lc| lc + crArray[i-1] - CS::one(),
+            |lc| lc + wArray[i] - CS::one(),
+            |lc| lc + CS::one() - crArray[i],
         );
     }
 
@@ -203,24 +214,23 @@ pub fn range<Scalar, CS>(mut cs: CS, input: &Range) -> Result<(), SynthesisError
         || "not_all_zeros=cr_n",
         |lc| lc + not_all_zeros,
         |lc| lc + CS::one(),
-        |lc| lc+crArray[crArray.len()-1],
+        |lc| lc + crArray[crArray.len() - 1],
     );
 
     cs.enforce(
         || "wn=less_or_equal*wn",
-        |lc| lc + wArray[wArray.len()-1],
+        |lc| lc + wArray[wArray.len() - 1],
         |lc| lc + (Scalar::from(less_or_equal), CS::one()),
-        |lc| lc+wArray[wArray.len()-1],
+        |lc| lc + wArray[wArray.len() - 1],
     );
 
     cs.enforce(
         || "wn*less_or_equal=less",
-        |lc| lc + wArray[wArray.len()-1],
+        |lc| lc + wArray[wArray.len() - 1],
         |lc| lc + not_all_zeros,
-        |lc| lc+(Scalar::from(less), CS::one()),
+        |lc| lc + (Scalar::from(less), CS::one()),
     );
     Ok(())
-}
 }
 
 
@@ -230,11 +240,23 @@ mod test {
     use rand::thread_rng;
     use ff::{PrimeField};
     use bls12_381::{Scalar};
-    use bellman::{Circuit, ConstraintSystem, SynthesisError};
-    use crate::helpers::range::Range;
+    use bellman::{
+        Circuit, ConstraintSystem, SynthesisError,
+        gadgets::test::TestConstraintSystem
+    };
+    use crate::helpers::range::{
+        less_or_equal
+    };
 
     #[test]
-    fn test_mpc_work() {
+    fn test_less_or_equal() {
+        let mut cs = TestConstraintSystem::<Scalar>::new();
+
+        less_or_equal(cs, (1, true), (2, false));
+    }
+
+    #[test]
+    fn test_range_mpc() {
         let mut rng = thread_rng();
         // Prepare circuit
         let constants = None;
@@ -273,9 +295,7 @@ mod test {
 
     impl<'a, S: PrimeField> Circuit<S> for TestDemo<'a, S> {
         fn synthesize<CS: ConstraintSystem<S>>(self, cs: &mut CS) -> Result<(), SynthesisError> {
-            Range::less_or_equal(cs,(self.xl.unwrap(),true),(self.xr.unwrap(),true))
+            less_or_equal(cs, (self.xl.unwrap(), true), (self.xr.unwrap(), true))
         }
     }
-
-
 }
